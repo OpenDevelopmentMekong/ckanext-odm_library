a7b35895240d27c6c656d6c0b1939e4b6ee10407
Alex Corbi <a.corbi@gmail.com>
2017-11-14 12:42:52 +0100

    Fixing templating for default issue

--- ckanext/odm_library/lib/odm_library_helper.py
+++ ckanext/odm_library/lib/odm_library_helper.py
@@ -25,9 +25,7 @@ def create_default_issue_library_record(pkg_info):
   ''' Uses CKAN API to add a default Issue as part of the vetting workflow for library records'''
   try:
 
-    extra_vars = {
-      't0': toolkit._("Thank you for uploading this item. Instructions about vetting system available on https://wiki.opendevelopmentmekong.net/partners:content_review#instructions_for_default_issue_on_library_records")
-    }
+    extra_vars = {}
 
     issue_message = render('messages/default_issue_library_record.txt',extra_vars=extra_vars)
 
